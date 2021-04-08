b3af3f9c58759ae1f0b2fe70da79a4553b1e572b
wisanggenib <bagus.wisanggeni4897@gmail.com>
2021-04-08 16:44:32 +0700

    modified content on menu

--- src/containers/menu/menu.container.js
+++ src/containers/menu/menu.container.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import styles from './menu.style';
 
+const _renderHeader = () => {
+  return (
+    <View>
+      <Text style={[styles.mainTitle, styles.bold]}>What Brings You</Text>
+      <Text style={[styles.mainTitle]}>to Silent Moon ?</Text>
+      <Text style={styles.desc}>choose a topic to focuse on:</Text>
+    </View>
+  );
+};
+
+const _renderContent = () => {
+  return (
+    <View style={styles.content}>
+      <View style={styles.contentWrapper}>
+        <View style={styles.listContent}>
+          <Text>This is Content</Text>
+        </View>
+        <View style={styles.listContent}>
+          <Text>This is Content</Text>
+        </View>
+        <View style={styles.listContent}>
+          <Text>This is Content</Text>
+        </View>
+        <View style={[styles.listContent]}>
+          <Text>This is Content</Text>
+        </View>
+        <View style={styles.listContent}>
+          <Text>This is Content</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 class Menu extends React.Component {
   render() {
     const text = 'Menu';
     return (
-      <View>
-        <Text> This is {text} Page</Text>
+      <View style={styles.wrapper}>
+        {_renderHeader()}
+        {_renderContent()}
       </View>
     );
   }
---
+++ src/containers/menu/menu.style.js
@@ -0,0 +1,45 @@
+import {Dimensions, StyleSheet} from 'react-native';
+import {Colors, Fonts} from '../../style';
+
+const {width} = Dimensions.get('window');
+
+export default StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  mainTitle: {
+    fontSize: Fonts.xxlarge,
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  desc: {
+    fontSize: Fonts.large,
+    color: Colors.gray,
+  },
+  content: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    marginTop: 10,
+  },
+  contentWrapper: {
+    backgroundColor: 'aqua',
+    width: '100%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'center',
+    alignContent: 'center',
+    paddingHorizontal: 10,
+  },
+  listContent: {
+    width: width * 0.4,
+    backgroundColor: 'red',
+    height: 100,
+    marginVertical: 10,
+    marginLeft: 5,
+    //    alignSelf: 'center',
+    marginRight: 'auto',
+  },
+});
