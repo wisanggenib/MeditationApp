ea29528e8f7e3db6e5ce7e56f8e0a99fe0fa09f6
wisanggenib <bagus.wisanggeni4897@gmail.com>
2021-04-21 10:47:27 +0700

    fix bug on custom mansory

--- src/components/mansory/mansory.style.js
+++ src/components/mansory/mansory.style.js
@@ -1,11 +1,19 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
+const {width} = Dimensions.get('window');
 
 export default StyleSheet.create({
   wrapper: {
     flex: 1,
     flexDirection: 'row',
     alignItems: 'stretch',
-    justifyContent: 'space-around',
+    justifyContent: 'space-evenly',
   },
-  paddingItem: {marginBottom: 10},
+  paddingItem: {
+    marginBottom: 10,
+    width: width * 0.44,
+    resizeMode: 'stretch',
+  },
+  contentWrap: {
+    padding: 10,
+  },
 });
